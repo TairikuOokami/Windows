b6f758caf869f5d44c7e1992d7b2b509a5da8b2d
TairikuOkami <81519712+TairikuOokami@users.noreply.github.com>
2021-04-25 12:28:39 +0200

    Update Microsoft Defender Disable.bat

--- Microsoft Defender Disable.bat
+++ Microsoft Defender Disable.bat
@@ -10,10 +10,7 @@ reg add "HKLM\Software\Microsoft\Windows Defender\Features" /v "TamperProtection
 rem https://technet.microsoft.com/en-us/itpro/powershell/windows/defender/set-mppreference
 rem https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-0290
 
-rem Exclusion in WD can be easily set with an elevated cmd, so that makes it super easy to damage any pc.
-rem WMIC /NAMESPACE:\\root\Microsoft\Windows\Defender PATH MSFT_MpPreference call Add ExclusionPath="xxxxxx
-
-rem To disable System Guard Runtime Monitor Broker
+rem To disable System Guard Runtime Monitor Broker (when disabled, it might cause BSOD Critical Process Died)
 rem reg add "HKLM\System\CurrentControlSet\Services\SgrmBroker" /v "Start" /t REG_DWORD /d "4" /f
 
 rem To disable Windows Defender Security Center include this
@@ -39,29 +36,30 @@ rem 0 - Disable Logging
 reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderApiLogger" /v "Start" /t REG_DWORD /d "0" /f
 reg add "HKLM\System\CurrentControlSet\Control\WMI\Autologger\DefenderAuditLogger" /v "Start" /t REG_DWORD /d "0" /f
 
-rem Disable WD Tasks
+rem Disable Tasks
 schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Disable
 schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable
 schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable
 schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable
 schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable
 
-rem Disable WD systray icon
+rem Disable systray icon
 reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "SecurityHealth" /f
 reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /f
 
-rem Remove WD context menu
+rem Remove context menu
 reg delete "HKCR\*\shellex\ContextMenuHandlers\EPP" /f
 reg delete "HKCR\Directory\shellex\ContextMenuHandlers\EPP" /f
 reg delete "HKCR\Drive\shellex\ContextMenuHandlers\EPP" /f
 
-rem Disable WD services
+rem Disable services (it will stop WdFilter.sys as well, better not to disable the driver by itself)
 rem reg add "HKLM\System\CurrentControlSet\Services\WdBoot" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\WdFilter" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\WdNisDrv" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\System\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d "4" /f
 
-rem You need to restart and run the .bat again to disable WD services (MsMpEng.exe) !!!!!
+rem You need to run twice or restart and run the .bat again to disable services (MsMpEng.exe) !!!!!
+rem If it does not work, install 3rd party AV temporarily, then run .bat and uninstall 3rd party AV.
 
 pause
