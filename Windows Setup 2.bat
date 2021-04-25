9b11e5c07e05cde61bdb60152427ab589dffbc9f
TairikuOkami <81519712+TairikuOokami@users.noreply.github.com>
2021-04-25 17:35:24 +0200

    Update Windows Setup 2.bat

--- Windows Setup 2.bat
+++ Windows Setup 2.bat
@@ -123,6 +123,17 @@ rem "HKCR\Wow6432Node\AppID\{0358b920-0ac7-461f-98f4-58e32cd89148}"
 rem "HKLM\Software\Wow6432Node\Classes\AppID\{3eb3c877-1f16-487c-9050-104dbcd66683}"
 rem "HKLM\Software\Wow6432Node\Classes\CLSID\{0358b920-0ac7-461f-98f4-58e32cd89148}"
 
+rem Disabled #heavy-ad-privacy-mitigations
+rem Disabled #edge-omnibox-ui-hide-steady-state-url-scheme
+rem Disabled #edge-omnibox-ui-hide-steady-state-url-trivial-subdomains
+rem Enabled #block-insecure-private-network-requests
+rem Enabled #disallow-doc-written-script-loads
+rem Enabled #enable-heavy-ad-intervention
+rem Enabled #enable-quic
+rem Enabled #enable-webrtc-hide-local-ips-with-mdns
+
+rem https://duckduckgo.com/?q=%s&k1=-1&k5=2&kah=sk-sk&kak=-1&kao=-1&kap=-1&kaq=-1&kau=-1&kav=1&kax=-1&kk=-1&t=h_&ia=web
+
 rem Enhancer for Youtube
 rem {"version":"2.0.104.8","settings":{"blur":0,"brightness":100,"contrast":100,"grayscale":0,"huerotate":0,"invert":0,"saturate":100,"sepia":0,"applyvideofilters":false,"backgroundcolor":"#000000","backgroundopacity":50,"blackbars":false,"blockads":true,"blockadsexceptforsubs":false,"blockautoplay":false,"blockhfrformats":false,"blockwebmformats":false,"cinemamode":false,"cinemamodewideplayer":false,"controlbar":{"active":true,"autohide":false,"centered":true,"position":"fixed"},"controls":[],"controlsvisible":false,"controlspeed":false,"controlspeedmousebutton":false,"controlvolume":false,"controlvolumemousebutton":false,"customcolors":{"--dimmer-text":"#cccccc","--hover-background":"#232323","--main-background":"#111111","--main-color":"#00adee","--main-text":"#eff0f1","--second-background":"#181818","--shadow":"#000000"},"customcssrules":"","customscript":"","customtheme":false,"darktheme":true,"date":1590884155282,"defaultvolume":false,"disableautoplay":false,"executescript":false,"expanddescription":false,"filter":"none","hidecardsendscreens":false,"hidechat":false,"hidecomments":false,"hiderelated":false,"ignoreplaylists":false,"ignorepopupplayer":true,"localecode":"en_US","localedir":"ltr","message":false,"miniplayer":false,"miniplayerposition":"_top-left","miniplayersize":"_400x225","newestcomments":false,"overridespeeds":false,"pauseforegroundtab":false,"pausevideos":false,"popuplayersize":"640x360","qualityembeds":"hd720","qualityembedsfullscreen":"hd1080","qualityplaylists":"hd1080","qualityplaylistsfullscreen":"hd1080","qualityvideos":"hd1080","qualityvideosfullscreen":"hd1080","reload":false,"reversemousewheeldirection":false,"selectquality":true,"selectqualityfullscreenoff":false,"selectqualityfullscreenon":false,"speed":1,"speedvariation":0.1,"stopvideos":false,"theatermode":false,"theme":"default-dark","themevariant":"youtube-deep-dark.css","update":1619098163598,"volume":50,"volumemultiplier":3,"volumevariation":5,"whitelist":"","wideplayer":false,"wideplayerviewport":false}}
 
