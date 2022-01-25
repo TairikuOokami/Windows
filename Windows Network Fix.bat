c4f1e22714c805f5a9430bba3ae307d37c29cf6f
TairikuOkami <81519712+TairikuOokami@users.noreply.github.com>
2022-01-25 09:14:59 +0100

    Update Windows Network Fix.bat

--- Windows Network Fix.bat
+++ Windows Network Fix.bat
@@ -1,135 +1,106 @@
 rem USE AT OWN RISK AS IS WITHOUT WARRANTY OF ANY KIND !!!!!
 
-rem Driver Easy - https://www.drivereasy.com
-rem Driver Store Explorer - https://github.com/lostindark/DriverStoreExplorer/releases
-rem HiBit Uninstaller - http://hibitsoft.ir
-rem Wise Disk Cleaner - http://www.wisecleaner.com/wise-disk-cleaner.html
-rem Wise Registry Cleaner - http://www.wisecleaner.com/wise-registry-cleaner.html
-rem Windows Drivers - http://www.catalog.update.microsoft.com
+rem https://support.microsoft.com/en-us/windows/fix-wi-fi-connection-issues-in-windows-9424a1f7-6a3b-65a6-4d78-7f07eee84d2c
+rem Windows Repair Toolbox - https://windows-repair-toolbox.com
+rem Network Optimization / TCP Optimizer - www.speedguide.net/downloads.php
+rem www.tenforums.com/network-sharing/2806-slow-network-throughput-windows-10-a.html#post553305
+rem https://www.tenforums.com/network-sharing/130285-tweak.html
 
-rem Disable Reserved Storage (7GB)
-Dism /Online /Set-ReservedStorageState /State:Disabled /Quiet /NoRestart
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\ReserveManager" /v "MiscPolicyInfo" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\ReserveManager" /v "PassedPolicy" /t REG_DWORD /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\ReserveManager" /v "ShippedWithReserves" /t REG_DWORD /d "0" /f
+rem Network Connection Status Indicator (NCSI) - HKLM\System\CurrentControlSet\Services\NlaSvc\Parameters\Internet
+reg add "HKLM\Software\Policies\Microsoft\Windows\NetworkConnectivityStatusIndicator" /v "NoActiveProbe" /t REG_DWORD /d "0" /f
+reg add "HKLM\System\CurrentControlSet\Services\NlaSvc\Parameters\Internet" /v "EnableActiveProbing" /t REG_DWORD /d "1" /f
 
-rem Open Explorer - Choose the desired View - View - Options - View - Apply to Folders - OK - Close Explorer ASAP
-reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags" /f
-reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /f
-reg delete "HKCU\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\Bags" /f
-reg delete "HKCU\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" /f
-reg delete "HKCU\Software\Microsoft\Windows\Shell\Bags" /f
-reg delete "HKCU\Software\Microsoft\Windows\Shell\BagMRU" /f
-reg delete "HKCU\Software\Microsoft\Windows\ShellNoRoam\Bags" /f
-reg delete "HKCU\Software\Microsoft\Windows\ShellNoRoam\BagMRU" /f
-reg add "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags\AllFolders\Shell" /v "FolderType" /t REG_SZ /d "NotSpecified" /f
+reg add "HKLM\System\CurrentControlSet\Services\BFE" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\System\CurrentControlSet\Services\Dnscache" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\System\CurrentControlSet\Services\MpsSvc" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\System\CurrentControlSet\Services\WinHttpAutoProxySvc" /v "Start" /t REG_DWORD /d "3" /f
 
-reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v "IconStreams" /f
-reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v "PastIconsStream" /f
+sc config Dhcp start= auto
+sc config DPS start= auto
+sc config lmhosts start= auto
+sc config NlaSvc start= auto
+sc config nsi start= auto
+sc config RmSvc start= auto
+sc config Wcmsvc start= auto
+sc config WdiServiceHost start= demand
+sc config Winmgmt start= auto
 
-rem Defrag HDD - Boot Windows USB - Repair - Troubleshoot - CMD - type/enter
-rem c:
-rem cd windows
-rem cd system32
-rem defrag c: /u
-rem https://s22.postimg.cc/df2t2gh8v/capture_06272018_231341.jpg
-rem https://s22.postimg.cc/lkkv0mkxd/capture_06272018_232206.jpg
+sc config NcbService start= demand
+sc config ndu start= demand
+sc config Netman start= demand
+sc config netprofm start= demand
+sc config WlanSvc start= auto
+sc config WwanSvc start= demand
 
-fsutil usn deletejournal /d /n c:
-chkdsk /scan
-ipconfig /flushdns
+net start DPS
+net start nsi
+net start NlaSvc
+net start Dhcp
+net start Wcmsvc
+net start RmSvc
 
-taskkill /im msi.exe /f
-taskkill /im wuauclt.exe /f
-taskkill /im sihclient.exe /f
-taskkill /im TiWorker.exe /f
-taskkill /im trustedinstaller.exe /f
-taskkill /im MoUsoCoreWorker.exe /f
-taskkill /im UsoClient.exe /f
-taskkill /im usocoreworker.exe /f
-net stop bits /y
-net stop cryptSvc /y
-net stop DoSvc /y
-net stop EventLog /y
-net stop msiserver /y
-net stop UsoSvc /y
-net stop winmgmt /y
-winmgmt /salvagerepository
-net stop wuauserv /y
+schtasks /Change /TN "Microsoft\Windows\DUSM\dusmtask" /Enable
 
-takeown /f "%WINDIR%\winsxs\pending.xml" /a
-icacls "%WINDIR%\winsxs\pending.xml" /grant:r Administrators:F /c
-del "%WINDIR%\winsxs\pending.xml" /s /f /q
+rem Disable adapter with index number 0-5 (most likely all), equals to ipconfig /release
+wmic path win32_networkadapter where index=0 call disable
+wmic path win32_networkadapter where index=1 call disable
+wmic path win32_networkadapter where index=2 call disable
+wmic path win32_networkadapter where index=3 call disable
+wmic path win32_networkadapter where index=4 call disable
+wmic path win32_networkadapter where index=5 call disable
 
-del "C:\$Recycle.bin" /s /f /q
-del "D:\$Recycle.bin" /s /f /q
-del "Z:\$Recycle.bin" /s /f /q
-del "%ALLUSERSPROFILE%\Application Data\Microsoft\Network\Downloader\qmgr*.dat" /s /f /q
-del "%ALLUSERSPROFILE%\Microsoft\Network\Downloader\qmgr*.dat" /s /f /q
-del "%LocalAppData%\Microsoft\Windows\WebCache" /s /f /q
-del "%LocalAppData%\Temp" /s /f /q
-del "%ProgramData%\USOPrivate\UpdateStore" /s /f /q
-del "%ProgramData%\USOShared\Logs" /s /f /q
-del "%temp%" /s /f /q
-del "%WINDIR%\Logs" /s /f /q
-del "%WINDIR%\Installer\$PatchCache$" /s /f /q
-del "%WINDIR%\SoftwareDistribution\Download" /s /f /q
-del "%WINDIR%\System32\LogFiles" /s /f /q
-del "%WINDIR%\System32\winevt\Logs" /s /f /q
-del "%WINDIR%\Temp" /s /f /q
-del "%WINDIR%\WinSxS\Backup" /s /f /q
+rem Timeout to let the network adapter recover
+timeout 5
 
-vssadmin delete shadows /for=c: /all /quiet
+rem Enable adapter with index number 0-5 (most likely all), equals to ipconfig /renew
+wmic path win32_networkadapter where index=0 call enable
+wmic path win32_networkadapter where index=1 call enable
+wmic path win32_networkadapter where index=2 call enable
+wmic path win32_networkadapter where index=3 call enable
+wmic path win32_networkadapter where index=4 call enable
+wmic path win32_networkadapter where index=5 call enable
 
-rem https://forums.mydigitallife.net/threads/windows-10-hotfix-repository.57050/page-622#post-1655591
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SideBySide\Configuration" /v "CBSLogCompress" /t "REG_DWORD" /d "1" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SideBySide\Configuration" /v "DisableComponentBackups" /t "REG_DWORD" /d "1" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SideBySide\Configuration" /v "DisableResetbase" /t "REG_DWORD" /d "1" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SideBySide\Configuration" /v "NumCBSPersistLogs" /t "REG_DWORD" /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SideBySide\Configuration" /v "SupersededActions" /t "REG_DWORD" /d "3" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\SideBySide\Configuration" /v "TransientManifestCache" /t "REG_DWORD" /d "1" /f
+rem advfirewall  - technet.microsoft.com/en-us/library/cc771046(v=ws.10).aspx
+rem arp - technet.microsoft.com/en-us/library/cc940107.aspx
+rem ipconfig - technet.microsoft.com/en-us/library/bb490921.aspx
+rem nbtstat - technet.microsoft.com/en-us/library/bb490938.aspx
+rem netsh - technet.microsoft.com/en-us/library/cc770948(v=ws.10).aspx
+rem route - technet.microsoft.com/en-us/library/bb490991.aspx
+rem support.microsoft.com/en-us/help/10741/windows-10-fix-network-connection-issues
 
-Dism /get-mountedwiminfo
-Dism /cleanup-mountpoints
-Dism /cleanup-wim
-Dism /Online /Cleanup-Image /StartComponentCleanup
+arp -d *
+route -f
+nbtstat -R
+nbtstat -RR
+netsh advfirewall reset
 
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Active Setup Temp Folders" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Content Indexer Cleaner" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\D3D Shader Cache" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Delivery Optimization Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Device Driver Packages" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Diagnostic Data Viewer database files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Downloaded Program Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\DownloadsFolder" /v "StateFlags6553" /t REG_DWORD /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Internet Cache Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Offline Pages Files" /v "StateFlags6553" /t REG_DWORD /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Old ChkDsk Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Previous Installations" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Recycle Bin" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\RetailDemo Offline Content" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Setup Log Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\System error memory dump files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\System error minidump files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Temporary Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Temporary Setup Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Thumbnail Cache" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Update Cleanup" /v "Autorun" /t REG_DWORD /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Update Cleanup" /v "StateFlags6553" /t REG_DWORD /d "0" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Upgrade Discarded Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\User file versions" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows Defender" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows Error Reporting Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows ESD installation files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows Upgrade Log Files" /v "StateFlags6553" /t REG_DWORD /d "2" /f
-rem cleanmgr /sageset:6553
-cleanmgr /sagerun:6553
+netcfg -d
+netsh winsock reset
+netsh int 6to4 reset all
+netsh int httpstunnel reset all
+netsh int ip reset
+netsh int isatap reset all
+netsh int portproxy reset all
+netsh int tcp reset all
+netsh int teredo reset all
+netsh branchcache reset
+ipconfig /release
+ipconfig /renew
 
-rem Cleanup done, you can close this window!
-timeout -1
+rem If you get "Access denied" message, take ownership of the following key in registry, then set Permissions for the current user to Allow Full Control or try to run BAT in the safe mode
+rem HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nsi\{eb004a00-9b1a-11d4-9123-0050047759bc}\26
 
-start "" /wait "%ProgramFiles(x86)%\Wise\Wise Disk Cleaner\WiseDiskCleaner.exe" -a -adv
-start "" /wait "%ProgramFiles(x86)%\Wise\Wise Registry Cleaner\WiseRegCleaner.exe" -a -all
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "ValidateAdminCodeSignatures" /t REG_DWORD /d "0" /f
-start "" /wait "D:\OneDrive\Soft\Windows Repair Toolbox\Downloads\Custom Tools\Added Custom Tools\Rapr.exe"
-start "" /wait "%ProgramFiles(x86)%\ADATA\SSD ToolBox\SSDToolBox.exe"
+rem Make sure, that no IPs are blocked in HOSTS, only those 2 entries should be there:
+rem #	127.0.0.1       localhost
+rem #	::1             localhost
+
+start "" /wait notepad %WINDIR%\System32\Drivers\Etc\Hosts
+
+rem Force restart in 1 minute
+shutdown /r /t 60
+
+rem To disable restart, type:
+
+rem shutdown /a
+
+pause
