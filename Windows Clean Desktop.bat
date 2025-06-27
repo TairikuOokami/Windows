65a51c51481d914f24b660210afde3ec3db345bd
TairikuOkami <81519712+TairikuOokami@users.noreply.github.com>
2025-06-27 07:39:58 +0200

    Update Windows Clean Desktop.bat

--- Windows Clean Desktop.bat
+++ Windows Clean Desktop.bat
@@ -24,26 +24,3 @@ md "%USERPROFILE%\Desktop"
 
 rd "Z:\Temp" /s /q
 md "Z:\Temp"
-
-rem Add Windows DoT
-netsh dns add encryption server=76.76.2.2 dothost=p2.freedns.controld.com:853 autoupgrade=yes udpfallback=no
-
-rem netsh dns add encryption server=193.110.81.0 dothost=zero.dns0.eu:853 autoupgrade=yes udpfallback=no
-rem netsh dns add encryption server=185.253.5.0 dothost=zero.dns0.eu:853 autoupgrade=yes udpfallback=no
-rem netsh dns add encryption server=45.90.28.99 dothost=FDDefine7Mini-xxxxxx.dns.nextdns.io:853 autoupgrade=yes udpfallback=no
-rem netsh dns add encryption server=45.90.30.99 dothost=FDDefine7Mini-xxxxxx.dns.nextdns.io:853 autoupgrade=yes udpfallback=no
-
-rem Add Windows DoT Manually
-rem reg add "HKLM\System\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers\45.90.28.99" /v "DotFlags" /t REG_DWORD /d "4" /f
-rem reg add "HKLM\System\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers\45.90.30.99" /v "DotFlags" /t REG_DWORD /d "4" /f
-rem reg add "HKLM\System\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers\45.90.28.99" /v "DotHost" /t REG_SZ /d "FDDefine7Mini-xxxxxx.dns.nextdns.io" /f
-rem reg add "HKLM\System\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers\45.90.30.99" /v "DotHost" /t REG_SZ /d "FDDefine7Mini-xxxxxx.dns.nextdns.io" /f
-rem reg add "HKLM\System\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers\45.90.28.99" /v "DotPort" /t REG_DWORD /d "853" /f
-rem reg add "HKLM\System\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers\45.90.30.99" /v "DotPort" /t REG_DWORD /d "853" /f
-
-rem Add DoH for Edge, to reset, remove the entry/policy
-rem https://adguard-dns.io/kb/general/dns-providers
-reg add "HKLM\Software\Policies\Microsoft\Edge" /v "BuiltInDnsClientEnabled" /t REG_DWORD /d "1" /f
-reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsMode" /t REG_SZ /d "secure" /f
-reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsTemplates" /t REG_SZ /d "https://freedns.controld.com/p2?" /f
-rem reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsTemplates" /t REG_SZ /d "https://zero.dns0.eu?" /f
