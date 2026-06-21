ce200e9d466edd812a19a1796435106f8a047fec
TairikuOkami <81519712+TairikuOokami@users.noreply.github.com>
2026-06-21 10:19:43 +0200

    Update Windows Clean Desktop.bat

--- Windows Clean Desktop.bat
+++ Windows Clean Desktop.bat
@@ -20,18 +20,17 @@ del "C:\Users\Tairi\OneDrive\Desktop\*" /s /f /q
 del "%USERPROFILE%\Desktop\*" /s /f /q
 rd "C:\Users\Tairi\OneDrive\Desktop" /s /q
 rd "%USERPROFILE%\Desktop" /s /q
-md "C:\Users\Tairi\OneDrive\Desktop"
 md "%USERPROFILE%\Desktop"
 
-del "Z:\Temp\*" /s /f /q
-rd "Z:\Temp" /s /q
-md "Z:\Temp"
+rem Add Windows DoH
+netsh dns add encryption server=94.140.14.14 dohtemplate=https://dns.adguard-dns.com/dns-query autoupgrade=yes udpfallback=no
+netsh dns add encryption server=94.140.15.15 dohtemplate=https://dns.adguard-dns.com/dns-query autoupgrade=yes udpfallback=no
 
 rem Add Windows DoT
-netsh dns set global doh=no
-netsh dns add global dot=yes ddr=no
-netsh dns set global dot=yes ddr=no
-reg add "HKLM\System\CurrentControlSet\Services\Dnscache\Parameters" /v "EnableAutoDoh" /t REG_DWORD /d "0" /f
+rem netsh dns set global doh=no
+rem netsh dns add global dot=yes ddr=no
+rem netsh dns set global dot=yes ddr=no
+rem reg add "HKLM\System\CurrentControlSet\Services\Dnscache\Parameters" /v "EnableAutoDoh" /t REG_DWORD /d "0" /f
 netsh dns add encryption server=94.140.14.14 dothost=dns.adguard-dns.com:853 autoupgrade=yes udpfallback=no
 netsh dns add encryption server=94.140.15.15 dothost=dns.adguard-dns.com:853 autoupgrade=yes udpfallback=no
 netsh dns add encryption server=94.140.14.14 dohtemplate=https://dns.adguard-dns.com/dns-query autoupgrade=yes udpfallback=no
@@ -39,9 +38,8 @@ netsh dns add encryption server=94.140.15.15 dohtemplate=https://dns.adguard-dns
 
 rem Add DoH for Edge, to reset, remove the entry/policy
 rem https://adguard-dns.io/kb/general/dns-providers
-reg add "HKLM\Software\Policies\Microsoft\Edge" /v "BuiltInDnsClientEnabled" /t REG_DWORD /d "1" /f
-reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsMode" /t REG_SZ /d "secure" /f
-reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsTemplates" /t REG_SZ /d "https://dns.adguard-dns.com/dns-query?" /f
+rem reg add "HKLM\Software\Policies\Microsoft\Edge" /v "BuiltInDnsClientEnabled" /t REG_DWORD /d "1" /f
+rem reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsMode" /t REG_SZ /d "secure" /f
+rem reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsTemplates" /t REG_SZ /d "https://dns.adguard-dns.com/dns-query?" /f
 
 ipconfig /flushdns
-
